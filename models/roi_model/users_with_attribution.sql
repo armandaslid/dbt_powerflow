511aa1682422bd366ac6d3aaa2e2949c17722d5f
Armandas Lidžius <armandaslid@gmail.com>
2025-02-10 19:10:30 +0000

    Added comment on users_with_attribution.sql

--- models/roi_model/users_with_attribution.sql
+++ models/roi_model/users_with_attribution.sql
@@ -3,7 +3,7 @@ SELECT
     registration_time,
     reg.device_id,
     attribution_time,
-    COALESCE(channel, 'organic') AS channel,
+    COALESCE(channel, 'organic') AS channel,            -- Replaces NULL values with 'organic' to indicate that the user was not acquired through paid advertising.
     COALESCE(campaign_id,'organic') AS campaign_id,
     COALESCE(attribution_cost, 0) AS attribution_cost
 FROM
